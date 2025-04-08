b5bae22169301b9b5a2813a82e20f60db71ec168
mimi-munshi <maryam.munshi@nacgroup.com>
2025-04-08 15:13:22 -0400

    Add files via upload
    fixed bugs

--- userUnitTestScript.ps1
+++ userUnitTestScript.ps1
@@ -3,51 +3,53 @@
 
 Function Search-User
 {
-   [string[]]$userNameList = Get-AdUser -Filter * | Select-Object Name 
+   [string[]]$userNameList = (Get-AdUser -Filter * ).Name
+   $objectList = (Get-AdUser -Filter *)
    [string]$name;
    [char]$check
+   [int]$count = 0;
    $name = Read-Host "Enter Name";
    for ($i = 0; $i -lt $userNameList.length; $i++)
    {
     if ($userNameList[$i] -match $name)
     {
+        $count++;
         
-        $check = Read-Host "Is this who are looking for? (y/n): " $userNameList[$i]
+        $check = Read-Host "Is this who are looking for?" $userNameList[$i] "(y/n) "
         if ($check -eq 'y' -or $check -eq 'Y')
         {
-            $bool = (Get-Enabled -user (Get-AdUser -Filter *))
+            
+            $bool = (Get-Enabled -User (Get-AdUser -Identity $objectList[$i].SamAccountName))
             if ($bool)
             {
-                Write-Host "User is Enabled"
+                Write-Host $userNameList[$i]" is Enabled"
+                
             }
             break;
         }
-        else 
-        {
-            Write-Host "User does not exist in Active Directory"
-        }
-        
-
-
 
         
     }
+   
 
    }
  
 
    
-   
+   if( $count -le 0)
+   {
+       Write-Host "User does not exist in Active Directory"
+   }
 
 
 }
 
 Function Get-Enabled
 {
-    param([System.Object[]]$user)
+    param([string]$User)
     
-    Write-Host $user.SamAccountName
-    return 5 -gt 0#(Get-AdUser -Identity $samName).Enabled
+    
+    return (Get-AdUser -Identity $User).Enabled
 
 }
 
