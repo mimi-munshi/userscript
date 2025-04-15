060cec17fc19cc5e4d181b13b9d68ec41ec6df85
mimi-munshi <maryam.munshi@nacgroup.com>
2025-04-15 14:45:15 -0400

    Add files via upload

--- userUnitTestScript.ps1
+++ userUnitTestScript.ps1
@@ -15,17 +15,26 @@ Function Search-User
     {
         $count++;
         
-        $check = Read-Host "Is this who are looking for?" $userNameList[$i] "(y/n) "
+        $check = Read-Host "Is this who you are looking for?" $userNameList[$i] "(y/n) "
         if ($check -eq 'y' -or $check -eq 'Y')
         {
             
             $bool = (Get-Enabled -User (Get-AdUser -Identity $objectList[$i].SamAccountName))
             if ($bool)
             {
-                Write-Host $userNameList[$i]" is Enabled"
-                
+                Write-Host $userNameList[$i]" is Enabled"           
             }
-            break;
+            else 
+            {
+                Write-Host $userNameList[$i] "is not Enabled"
+            }
+                Write-Host "Account Name: " -NoNewline; Get-UserAccountName -User $objectList[$i]
+                Write-Host "Employee Name: " -NoNewline; Get-EmployeeName -User $objectList[$i]
+                Write-Host "Employee Number: " -NoNewline; Get-EmployeeNumber -User $objectList[$i]
+                Write-Host "Job Title: " -NoNewline; Get-JobTitle -User $objectList[$i]
+                Write-Host "Employee Phone: " -NoNewline; Get-EmployeePhone -User $objectList[$i]
+                Write-Host "Employee Manager: " -NoNewline; Get-Manager -User $objectList[$i]
+                break;
         }
 
         
@@ -53,4 +62,44 @@ Function Get-Enabled
 
 }
 
+Function Get-UserAccountName
+{
+    param([System.Object]$User)
+    return $User.UserPrincipalName
+
+}
+
+Function Get-EmployeeName
+{
+    param([System.Object]$User)
+    return $User.Name
+}
+
+Function Get-EmployeeNumber
+{
+    param([System.Object]$User)
+    return $User.SID
+}
+
+Function Get-JobTitle
+{
+    param([System.Object]$User)   
+    return (Get-AdUser -Identity $User.SamAccountName -Properties SamAccountName,Title).Title
+}
+
+Function Get-EmployeePhone
+{
+    param([System.Object]$User)   
+    return (Get-AdUser -Identity $User.SamAccountName -Properties homephone).homephone
+}
+
+Function Get-Manager
+{
+    param([System.Object]$User)
+    $string = (Get-AdUser -Identity $User.SamAccountName -Properties manager).manager
+    $part = ($string -split ',')[0]
+    $substring = $part.Substring(3)
+    return $substring
+
+}
 Search-User
